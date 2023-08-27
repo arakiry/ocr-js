82d04793dddf2a9562554eb8f28720110d16fcbe
arakiry <aryusei0206@gmail.com>
2023-08-27 22:56:11 +0900

    fix: recognizeのoutputにblocksを追加

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
 import React, { useEffect, useState } from 'react';
-// import { createWorker } from 'tesseract.js';
-import Tesseract from 'tesseract.js';
+import { createWorker } from 'tesseract.js';
+// import Tesseract from 'tesseract.js';
 
 import './App.css';
 
-const readImage = async (imageUrl) => {
-  const { data: { text } } = await Tesseract.recognize(
-    imageUrl,
-    'jpn',
-    // { logger: m => console.log(m) }
-  )
-  console.log(text);
-  return text;
-};
-
-// const doOCR = async (imageUrl) => {
-//   const worker = createWorker({
-//     logger: m => console.log(m),
-//   });
-//   await worker.load(imageUrl);
-//   await worker.loadLanguage('eng');
-//   await worker.initialize('eng');
-//   const { data: { text } } = await worker.recognize();
+// const readImage = async (imageUrl) => {
+//   const { data: { text } } = await Tesseract.recognize(
+//     imageUrl,
+//     'jpn',
+//     // { rectangle: { top: 0, left: 0, width: 100, height: 100 } },
+//     output
+//     // { logger: m => console.log(m) }
+//   )
+//   console.log(text);
 //   return text;
 // };
+
+const doOCR = async (imageUrl) => {
+  const worker = await createWorker({
+    // logger: m => console.log(m),
+  });
+  await worker.loadLanguage('eng');
+  await worker.initialize('eng');
+  const { data: { text, blocks } } = await worker.recognize(imageUrl);
+  console.log(text);
+  console.log(blocks);
+  return text;
+};
 
 function App() {
   const [ocr, setOcr] = useState('Recognizing...');
   useEffect(() => {
     (async() => {
-      // doOCR('https://tesseract.projectnaptha.com/img/eng_bw.png').then((text) => { setOcr(text); });
-      readImage('https://tesseract.projectnaptha.com/img/eng_bw.png').then((text) => { setOcr(text); });
+      doOCR('https://tesseract.projectnaptha.com/img/eng_bw.png').then((text) => { setOcr(text); });
+      // readImage('https://tesseract.projectnaptha.com/img/eng_bw.png').then((text) => { setOcr(text); });
     })();
   }, [ocr]);
   return (
